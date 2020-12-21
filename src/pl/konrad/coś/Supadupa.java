2c5d079ab805041d414e74288229988ec7b96bde
konrad <kondorro.suchy@gmail.com>
2020-12-21 20:33:21 +0100

    test

---
+++ src/pl/konrad/coś/Supadupa.java
@@ -0,0 +1,26 @@
+//package pl.konrad.coś;
+//
+//public class Supadupa {
+//    int burger;
+//    int frytki;
+//
+//    public Supadupa(int burger, int frytki) {
+//        this.burger = burger;
+//        this.frytki = frytki;
+//    }
+//
+//    public void money() {
+//        if (burger <= 1 && frytki <= 1) {
+//            System.out.println("Za mało jesz");
+//        } else if (burger <= 3 && burger >= 1 || frytki <= 3 && frytki >= 3) {
+//            System.out.println("jesz w sam raz");
+//        } else if (burger <= 5 && burger >= 3 || frytki <= 5 && frytki >= 3) {
+//            System.out.println("jesz za duzo");
+//        } else if (burger > 5  || frytki > 5 ){
+//            System.out.println("idź do lekarza");
+//
+//        }
+//
+//
+//    }
+//}
